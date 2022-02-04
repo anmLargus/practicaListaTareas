e45dc45a88e64f8d9b0187b7db4648ce876ee1cc
anmLargus <andresnm29@gmail.com>
2022-02-03 23:33:00 -0300

    deleteTask da error 404. Codigo esta bien compilado

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -18,4 +18,10 @@ export class TaskService {
 
     return this.http.get<Task[]>(this.apiUrl)
   }
+
+  deleteTask(task: Task) {
+    const url = '${this.apiUrl}/{task.id}'
+    return this.http.delete<Task[]>(url)
+  }
+
 }
